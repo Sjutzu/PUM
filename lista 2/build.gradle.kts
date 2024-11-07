cf8229ce12f5c0113c499f742a92a29acc77ca94
Sjutzu <daniel.zentlik6@gmail.com>
2024-11-07 12:39:44 +0100

    jetpacknavigation
    start of the project

---
+++ lista 2/build.gradle.kts
@@ -0,0 +1,17 @@
+buildscript { // przed blokiem plugins
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+    }
+}
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
